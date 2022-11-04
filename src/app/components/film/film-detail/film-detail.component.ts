059dc2293b9665454a03bce52ede49b203e8ab38
CapibaraNonimo <adrianarnaizcano@gmail.com>
2022-11-04 14:26:42 +0100

    avanzando en el detalle de película

--- src/app/components/film/film-detail/film-detail.component.ts
+++ src/app/components/film/film-detail/film-detail.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {MovieService} from '../../../services/movie.service';
-import {ActivatedRoute, RouterModule} from '@angular/router';
-import {Movie} from '../../../models/interfaces/movie/movie.interface';
+import {ActivatedRoute} from '@angular/router';
 import {MovieDetailsResponse} from '../../../models/interfaces/movie/movie-details.interface';
+import {ReleaseDateResponse} from '../../../models/interfaces/movie/release-date.interface';
 
 @Component({
   selector: 'app-film-detail',
@@ -11,15 +11,20 @@ import {MovieDetailsResponse} from '../../../models/interfaces/movie/movie-detai
 })
 export class FilmDetailComponent implements OnInit {
   film: MovieDetailsResponse;
+  releaseDate: ReleaseDateResponse
 
   constructor(private movieService: MovieService, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.movieService.getMovieDetails(params.id).subscribe(response => {
-        this.film = response;
+      this.movieService.getMovieDetails(params.id).subscribe(details => {
+        this.film = details;
         document.getElementById('bImage').style.backgroundImage = `url(${this.getImage(this.film.backdrop_path)})`;
+
+        this.movieService.getMovieReleaseDate(params.id).subscribe(releaseDate => {
+          this.releaseDate = releaseDate;
+        })
       });
     });
   }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { MovieResponse } from '../models/interfaces/movie/movie.interface';
 import {MovieDetailsResponse} from '../models/interfaces/movie/movie-details.interface';
+import {ReleaseDateResponse} from '../models/interfaces/movie/release-date.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,9 @@ export class MovieService {
 
   public getMovieDetails(movieId: number): Observable<MovieDetailsResponse> {
     return this.http.get<MovieDetailsResponse>(`${environment.API_BASE_URL}/movie/${movieId}?api_key=${environment.API_KEY_M}`)
+  }
+
+  public getMovieReleaseDate(movieId: number): Observable<ReleaseDateResponse> {
+    return this.http.get<ReleaseDateResponse>(`${environment.API_BASE_URL}/movie/${movieId}/release_dates?api_key=${environment.API_KEY_M}`)
   }
 }
